27663876daee2581541e8dee48eeaa1bb7fe4deb
Ewan Brain <ewan.brain@gmail.com>
2021-02-23 22:25:01 +0000

    inital commit

---
+++ src/test/java/com/codeclan/example/filemanager/FilemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.filemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
